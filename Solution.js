9cfd35f6dfdb31e99b3ab8cfdd4d889b9aed34b6
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-26 22:03:23 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,110 @@
+
+// const {MinPriorityQueue} = require('@datastructures-js/priority-queue');
+
+/**
+ * @param {number} numberOfCities
+ * @param {number[][]} roads
+ * @param {number[]} appleCost
+ * @param {number} multiplierForReturnJourney
+ * @return {number[]}
+ */
+var minCost = function (numberOfCities, roads, appleCost, multiplierForReturnJourney) {
+    this.numberOfCities = numberOfCities;
+    this.multiplierForReturnJourney = multiplierForReturnJourney;
+    const undirectedGraph = createUndirectedGraph(roads);
+    return findMinCostForAllApplesByMultisourceDijkstraSearch(undirectedGraph, appleCost);
+};
+
+/**
+ * @param {number} toCityID
+ * @param {number} cost
+ */
+function Road(toCityID, cost) {
+    this.toCityID = toCityID;
+    this.cost = cost;
+}
+
+/**
+ * @param {number} ID
+ * @param {number} costFromStart
+ */
+function City(ID, costFromStart) {
+    this.ID = ID;
+    this.costFromStart = costFromStart;
+}
+
+/**
+ * @param {Road[][]} undirectedGraph
+ * @param {number[]} appleCost
+ * @return {number[]}
+ */
+function findMinCostForAllApplesByMultisourceDijkstraSearch(undirectedGraph, appleCost) {
+    const minHeapCities = createMinHeapCities(appleCost);
+    const minAppleCost = createArrayMinAppleCost(appleCost);
+
+    while (!minHeapCities.isEmpty()) {
+
+        const city = minHeapCities.dequeue();
+        if (city.costFromStart > minAppleCost[city.ID]) {
+            continue;
+        }
+
+        for (let road of undirectedGraph[city.ID]) {
+            const totalAppleCost = calculateTotalAppleCost(city, road);
+            if (minAppleCost[road.toCityID] > totalAppleCost) {
+                minAppleCost[road.toCityID] = totalAppleCost;
+                minHeapCities.enqueue(new City(road.toCityID, minAppleCost[road.toCityID]));
+            }
+        }
+    }
+    return minAppleCost;
+}
+
+/**
+ * @param {number[][]} roads
+ * @return {Road[][]}
+ */
+function createUndirectedGraph(roads) {
+    const undirectedGraph = Array.from(new Array(this.numberOfCities), () => new Array());
+
+    for (let [from, to, cost] of roads) {
+        // from - 1 and to - 1
+        // transition from one-based to zero-based city IDs
+        undirectedGraph[from - 1].push(new Road(to - 1, cost));
+        undirectedGraph[to - 1].push(new Road(from - 1, cost));
+    }
+    return undirectedGraph;
+}
+
+/**
+ * @param {number[]} appleCost
+ * @return {MinPriorityQueue<City>}
+ */
+function createMinHeapCities(appleCost) {
+    const minHeapCities = new MinPriorityQueue({compare: (x, y) => x.costFromStart - y.costFromStart});
+    for (let i = 0; i < this.numberOfCities; ++i) {
+        minHeapCities.enqueue(new City(i, appleCost[i]));
+    }
+    return minHeapCities;
+}
+
+/** 
+ * @param {number[]} appleCost
+ * @return {number[]}
+ */
+function createArrayMinAppleCost(appleCost) {
+    const minCostPaths = new Array(this.numberOfCities);
+    for (let i = 0; i < this.numberOfCities; ++i) {
+        minCostPaths[i] = appleCost[i];
+    }
+    return minCostPaths;
+}
+
+/**
+ * @param {City} city
+ * @param {Road} road
+ * @return {number}
+ */
+function calculateTotalAppleCost(city, road) {
+    return city.costFromStart + road.cost * (1 + this.multiplierForReturnJourney);
+}
