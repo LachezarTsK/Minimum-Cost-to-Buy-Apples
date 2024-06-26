f6d01cbb9f7dc34a4d4b42c5b85e2a240c212e3d
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-06-26 22:30:53 +0200

    Update Solution.kt

--- Solution.kt
+++ Solution.kt
@@ -10,36 +10,33 @@ class Solution {
     private var multiplierForReturnJourney = 0
 
     fun minCost(numberOfCities: Int, roads: Array<IntArray>, appleCost: IntArray, multiplierForReturnJourney: Int): LongArray {
-        this.numberOfCities = numberOfCities;
-        this.multiplierForReturnJourney = multiplierForReturnJourney;
-        val undirectedGraph = createUndirectedGraph(roads);
-        return findMinCostForAllApplesByMultisourceDijkstraSearch(undirectedGraph, appleCost);
+        this.numberOfCities = numberOfCities
+        this.multiplierForReturnJourney = multiplierForReturnJourney
+        val undirectedGraph = createUndirectedGraph(roads)
+        return findMinCostForAllApplesByMultisourceDijkstraSearch(undirectedGraph, appleCost)
     }
 
-    private fun findMinCostForAllApplesByMultisourceDijkstraSearch(
-        undirectedGraph: Array<ArrayList<Road>>,
-        appleCost: IntArray
-    ): LongArray {
-        val minHeapCities = createMinHeapCities(appleCost);
-        val minAppleCost = createArrayMinAppleCost(appleCost);
+    private fun findMinCostForAllApplesByMultisourceDijkstraSearch(undirectedGraph: Array<ArrayList<Road>>, appleCost: IntArray): LongArray {
+        val minHeapCities = createMinHeapCities(appleCost)
+        val minAppleCost = createArrayMinAppleCost(appleCost)
 
         while (!minHeapCities.isEmpty()) {
 
-            val city = minHeapCities.poll();
+            val city = minHeapCities.poll()
             if (city.costFromStart > minAppleCost[city.ID]) {
-                continue;
+                continue
             }
 
             for (road in undirectedGraph[city.ID]) {
-                val totalAppleCost = calculateTotalAppleCost(city, road);
+                val totalAppleCost = calculateTotalAppleCost(city, road)
 
                 if (minAppleCost[road.toCityID] > totalAppleCost) {
-                    minAppleCost[road.toCityID] = totalAppleCost;
-                    minHeapCities.add(City(road.toCityID, minAppleCost[road.toCityID]));
+                    minAppleCost[road.toCityID] = totalAppleCost
+                    minHeapCities.add(City(road.toCityID, minAppleCost[road.toCityID]))
                 }
             }
         }
-        return minAppleCost;
+        return minAppleCost
     }
 
     private fun createUndirectedGraph(roads: Array<IntArray>): Array<ArrayList<Road>> {
@@ -48,21 +45,21 @@ class Solution {
         for (road in roads) {
             // road[0] - 1 and road[1] - 1
             // transition from one-based to zero-based city IDs
-            val from = road[0] - 1;
-            val to = road[1] - 1;
-            val cost = road[2];
-            undirectedGraph[from].add(Road(to, cost));
-            undirectedGraph[to].add(Road(from, cost));
+            val from = road[0] - 1
+            val to = road[1] - 1
+            val cost = road[2]
+            undirectedGraph[from].add(Road(to, cost))
+            undirectedGraph[to].add(Road(from, cost))
         }
-        return undirectedGraph;
+        return undirectedGraph
     }
 
     private fun createMinHeapCities(appleCost: IntArray): PriorityQueue<City> {
         val minHeapCities = PriorityQueue<City> { x, y -> x.costFromStart.compareTo(y.costFromStart) }
         for (i in 0..<numberOfCities) {
-            minHeapCities.add(City(i, appleCost[i].toLong()));
+            minHeapCities.add(City(i, appleCost[i].toLong()))
         }
-        return minHeapCities;
+        return minHeapCities
     }
 
     private fun createArrayMinAppleCost(appleCost: IntArray): LongArray {
